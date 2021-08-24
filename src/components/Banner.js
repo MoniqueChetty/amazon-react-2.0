bcde5643eca9f7a53824ba4b30807f8f36cbcdc8
Monique Chetty <55020016+MoniqueChetty@users.noreply.github.com>
2021-08-24 16:24:51 +0200

    test img

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -17,24 +17,24 @@ function Banner() {
       >
         <div className="c">
           {/* <img src="https://links.papareact.com/gi1" loading="lazy" /> */}
-          <img
+          {/* <img
             src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_TallHero_Gamers_en_US_2x._CB667161802_.jpg"
             loading="lazy"
-          />
+          /> */}
         </div>
         <div className="c">
           {/* <img src="https://links.papareact.com/6ff" loading="lazy" /> */}
-          <img
+          {/* <img
             src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Computers_2x._CB432469748_.jpg"
             loading="lazy"
-          />
+          /> */}
         </div>
         <div className="c">
           {/* <img src="https://links.papareact.com/7ma" loading="lazy" /> */}
-          <img
+          {/* <img
             loading="lazy"
             src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Toys_en_US_2x._CB431858162_.jpg"
-          />
+          /> */}
         </div>
       </Carousel>
     </div>
